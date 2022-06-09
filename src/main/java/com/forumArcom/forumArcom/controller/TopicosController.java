8a442326612650ee0968bceca06b4563d4b2945f
iwfet <j13vvc@gmail.com>
2022-06-09 17:44:13 -0300

    novas coisas

--- src/main/java/com/forumArcom/forumArcom/controller/TopicosController.java
+++ src/main/java/com/forumArcom/forumArcom/controller/TopicosController.java
@@ -1,16 +1,23 @@
 package com.forumArcom.forumArcom.controller;
 
+import com.forumArcom.forumArcom.controller.dto.TopicoDto;
+import com.forumArcom.forumArcom.modelo.Curso;
 import com.forumArcom.forumArcom.modelo.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    public List<Topico> lista(){
+    public List<TopicoDto> lista(){
+        Topico topico =new Topico("Dúvida","Duvida spring",new Curso("Spring","Progarmaço"));
+        return TopicoDto.convert(Arrays.asList(topico,topico,topico));
 
     }
 }
